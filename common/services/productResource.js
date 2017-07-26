d4b16c374f5bac742e163aaad10ebc82ca5af5f6
ixtlarle <ixtlarle@my.dom.edu>
2017-07-26 18:55:29 -0500

    retrieving data from backend web service and how to intercept the outgoing webserivce calls and mock results...created another module and added a dependencie to app.js..the newly added module makes calls to server.

---
+++ common/services/productResource.js
@@ -0,0 +1,15 @@
+// iife to prevent using global variable
+(
+    function(){
+        "use strict";
+        angular
+            .module("common.services")
+            .factory("productResource",
+            ["$resource",
+            productResource]);
+
+        function productResource($resource){
+            return $resource("/api/products/:productId")
+        };
+    }
+);
