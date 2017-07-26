a65418032cdd63cfb90c6bb7648a0db99f109745
ixtlarle <ixtlarle@my.dom.edu>
2017-07-26 01:46:10 -0500

    Adding files in project 'AcmeWAngularJs' to Git...

---
+++ app/app.js
@@ -0,0 +1,7 @@
+/*created by arley on 7/26/17*/
+//declared module and assigned the result to a variable used setter syntax; no dependencies
+//IIFE:immediately invoked function expression js pattern that helps prevent global declarations also called self executing anonymus function.
+(function(){
+    "use strict";
+var app = angular.module("productManagement",{})
+}());
