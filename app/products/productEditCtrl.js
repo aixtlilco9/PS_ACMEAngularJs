c84776a5bcdcfb21bf6c52cc2855e6797a514d1e
ixtlarle <ixtlarle@my.dom.edu>
2017-08-06 19:03:51 -0500

    added toastr to show a notification which shows up when the save is succesful

--- app/products/productEditCtrl.js
+++ app/products/productEditCtrl.js
@@ -20,16 +20,33 @@
             $event.stopPropagation();
 
             vm.opened = !vm.opened;
-        };
+        }
 
         vm.submit = function () {
-            vm.product.$save();
-        };
+            vm.product.$save(function(){
+                toastr.success("Save Successful");
+            });
+        }
 
         vm.cancel = function () {
-            //can use $state.go to naviagete to another state
+            //can use $state.go to naviagete to
             $state.go('productList');
-        };
+        }
+
+        vm.addTags = function(tags){
+            if(tags){
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array): array;
+                vm.newTags = "";
+            }
+            else{
+                alert("Please enter one of more tags seperated by commas");
+            }
+        }
+
+        vm.removeTag = function(idx){
+            vm.product.tags.splice(idx,1)
+        }
     }
 }
 ());
