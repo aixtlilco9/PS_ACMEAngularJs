f172a14a501164d50b74fb0f4c74a643aee2963a
ixtlarle <ixtlarle@my.dom.edu>
2017-08-06 22:56:19 -0500

    made is so that the form cannot be subbmited with errors as error messages were shown but it could still be saved so we prevented this by implementing the logic in controller..could of lso used the ng-disabled="form.%invalid" attribute in the submit button for the form

--- app/products/productEditCtrl.js
+++ app/products/productEditCtrl.js
@@ -22,10 +22,15 @@
             vm.opened = !vm.opened;
         }
 
-        vm.submit = function () {
-            vm.product.$save(function(){
-                toastr.success("Save Successful");
-            });
+        vm.submit = function (isValid) {
+           if(isValid){
+               vm.product.$save(function(){
+                   toastr.success("Save Successful");
+               })
+           }
+           else{
+               alert("Please correct the validation errors first.");
+           }
         }
 
         vm.cancel = function () {
